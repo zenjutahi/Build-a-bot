76cfb62d2e59ddbe61c215f4155794f52afea7c4
Zenjutahi <zenjutahi@gmail.com>
2019-01-31 18:35:08 +0300

    [feature] - styling links based on active route using '.router-link-active' in css

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,14 @@
       <nav>
         <ul>
           <li class="nav-item">
-            <router-link class="nav-link" :to="{name: 'Home'}">
+            <router-link class="nav-link" :to="{name: 'Home'}" exact>
               <img class="logo" src="./assets/build-a-bot-logo.png" />
               Build-a-Bot!
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" :to="{name: 'Build'}" exact>
+              Build
             </router-link>
           </li>
         </ul>
@@ -62,5 +67,8 @@ ul {
 .nav-link {
   text-decoration: none;
   color: inherit;
+}
+.router-link-active {
+  color: white;
 }
 </style>
