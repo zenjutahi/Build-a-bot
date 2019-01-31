ec58e57a1010fbb239702a72d7fb03847c87e64e
Zenjutahi <zenjutahi@gmail.com>
2019-01-31 19:36:27 +0300

    [feature] - validating props

--- src/parts/PartInfo.vue
+++ src/parts/PartInfo.vue
@@ -12,7 +12,15 @@ import parts from '../data/parts';
 
 export default {
   name: 'PartInfo',
-  props: ['partType', 'id'],
+  props: {
+    partType: { type: String},
+    id: {
+      type: [Number, String],
+      validator(value) {
+        return Number.isInteger(Number(value));
+      },
+    },
+  },
   computed: {
     part() {
       const { partType, id } = this;
