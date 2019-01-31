b0c8488a93e79e79de85620db8da670cd0c9fa04
Zenjutahi <zenjutahi@gmail.com>
2019-01-31 19:32:39 +0300

    [feature] - decoupling the componects from the routes using props

--- src/parts/PartInfo.vue
+++ src/parts/PartInfo.vue
@@ -12,9 +12,10 @@ import parts from '../data/parts';
 
 export default {
   name: 'PartInfo',
+  props: ['partType', 'id'],
   computed: {
     part() {
-      const { partType, id } = this.$route.params;
+      const { partType, id } = this;
       return parts[partType].find(part => part.id === +id);
     },
   },
