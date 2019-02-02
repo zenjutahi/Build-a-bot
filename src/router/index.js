27fda4feef141bc2e95ebac5c6f10669198f2dfc
Zenjutahi <zenjutahi@gmail.com>
2019-02-02 10:39:31 +0300

    [chore] using navigation guards to prevent pages loading

--- src/router/index.js
+++ src/router/index.js
@@ -61,5 +61,9 @@ export default new Router({
     name: 'Parts',
     component: PartInfo,
     props: true,
+    beforeEnter(to, from, next) {
+      const isValidId = Number.isInteger(Number(to.params.id));
+      next(isValidId);
+    },
   }],
 });
