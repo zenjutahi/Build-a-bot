3d2562db39aa5c28aac53e98ca1dbb6ed51a5a3d
Zenjutahi <zenjutahi@gmail.com>
2019-02-02 10:51:10 +0300

    [chore] using class based navigation guards

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -77,10 +77,19 @@ import CollapsibleSection from '../shared/CollapsibleSection.vue';
 
 export default {
   name: 'RobotBuilder',
+  beforeRouteLeave(to, from, next) {
+    if (this.addedToCart) {
+      next(true);
+    } else {
+      const response = confirm('You have not added your Robot to your cart, are you sure you want to leave.');
+      next(response);
+    }
+  },
   components: { PartSelector, CollapsibleSection },
   data() {
     return {
       availableParts,
+      addedToCart: false,
       cart: [],
       selectedRobot: {
         head: {},
@@ -108,6 +117,7 @@ export default {
       const cost = robot.head.cost + robot.leftArm.cost + robot.torso.cost
         + robot.rightArm.cost + robot.base.cost;
       this.cart.push(Object.assign({}, robot, { cost }));
+      this.addedToCart = true;
     },
   },
 };
