a454d9fefdc6a6ee2dc3c0e7898499d98cf16e55
Zenjutahi <zenjutahi@gmail.com>
2019-02-02 11:22:01 +0300

    [feature] - changing vuex states with mutations

--- src/store/index.js
+++ src/store/index.js
@@ -4,5 +4,12 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  
+  state: {
+    cart: [],
+  },
+  mutations: {
+    addRobotToCart(state, robot) {
+      state.cart.push(robot)
+    },
+  },
 });
