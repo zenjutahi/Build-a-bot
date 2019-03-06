eb1abb5240a66db0195afbc862f586344fa9c120
Zenjutahi <zenjutahi@gmail.com>
2019-03-06 11:36:46 +0300

    [chore] add axios to vue configuration

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8081/api/parts',
+        target: 'http://localhost:8081',
         changeOrigin: true,
       },
     },
