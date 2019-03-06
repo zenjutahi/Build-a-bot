7ae08be51e1b07e411668168e87740a7f2c806a8
Zenjutahi <zenjutahi@gmail.com>
2019-03-06 11:36:01 +0300

    [feature] create updateParts mutation

--- src/store/index.js
+++ src/store/index.js
@@ -7,15 +7,21 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     cart: [],
+    parts: null,
   },
   mutations: {
     addRobotToCart(state, robot) {
       state.cart.push(robot);
     },
+    updateParts(state, parts) {
+      state.parts = parts;
+    },
   },
   actions: {
-    getParts({ commit }){
-      axios.get();
+    getParts({ commit }) {
+      axios.get('/api/parts')
+        .then(result => commit('updateParts', result.data))
+        .catch(console.error);
     },
   },
   getters: {
