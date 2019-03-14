becef26dc8007685066045882ce6139980d7cf15
Zenjutahi <zenjutahi@gmail.com>
2019-03-14 13:37:17 +0300

    [feature] - returning promises from actions

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -103,7 +103,8 @@ export default {
       const robot = this.selectedRobot;
       const cost = robot.head.cost + robot.leftArm.cost + robot.torso.cost
         + robot.rightArm.cost + robot.base.cost;
-      this.$store.dispatch('addRobotToCart', Object.assign({}, robot, { cost }));
+      this.$store.dispatch('addRobotToCart', Object.assign({}, robot, { cost }))
+        .then(() => this.$router.push('/cart'));
       this.addedToCart = true;
     },
   },
