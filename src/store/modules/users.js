469ba1d893342e43d68a75e3f792667ca5f942ec
Zenjutahi <zenjutahi@gmail.com>
2019-03-18 22:12:50 +0300

    [chore] - namespacing modules

--- src/store/modules/users.js
+++ src/store/modules/users.js
@@ -17,5 +17,8 @@ export default {
         .then(result => commit('updateCurrentUser', result.data))
         .catch(console.error);
     },
+    addRobotToCart() {
+      console.log("hey hey")
+    },
   },
 };
